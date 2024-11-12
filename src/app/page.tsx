395b71a64d556498e6bf1f2472c628fe14be505b
Dominic <dominic2401@gmail.com>
2024-11-12 18:38:43 +0900

    Revert "Added mobile layout"
    This reverts commit 68c670298f1501a7a56e6c84af76f19c409e301f.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-/** @format */
-
 "use client";
 
 import { Provider, createStore } from "jotai";
@@ -10,25 +8,25 @@ import { Container, Section, Heading, Flex } from "@radix-ui/themes";
 const jotaiStore = createStore();
 
 export default function Home() {
-	const callObject = useCallObject({});
+  const callObject = useCallObject({});
 
-	return (
-		<Provider store={jotaiStore}>
-			<DailyProvider callObject={callObject} jotaiStore={jotaiStore}>
-				<Container size="3" p="4" id="app">
-					<Flex direction="column" gap="3">
-						<Section size="2">
-							<Heading align="center" as="h1">
-								Daily Network Test
-							</Heading>
-						</Section>
+  return (
+    <Provider store={jotaiStore}>
+      <DailyProvider callObject={callObject} jotaiStore={jotaiStore}>
+        <Container size="3" id="app">
+          <Flex direction="column" gap="3">
+            <Section size="2">
+              <Heading align="center" as="h1">
+                Daily Network Test
+              </Heading>
+            </Section>
 
-						<Section size="2" style={{ textAlign: "center" }}>
-							<App />
-						</Section>
-					</Flex>
-				</Container>
-			</DailyProvider>
-		</Provider>
-	);
+            <Section size="2" style={{ textAlign: "center" }}>
+              <App />
+            </Section>
+          </Flex>
+        </Container>
+      </DailyProvider>
+    </Provider>
+  );
 }
